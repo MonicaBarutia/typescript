e24e778d067df93a5e6c43ee9cfaa958236167a8
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-16 17:27:28 +0300

    Array type.

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 // const person: {
 //     name: string;
 //     age: number;
+//     hobbies: string[];
 // } = {
 var person = {
     name: 'Monica',
-    age: 25
+    age: 25,
+    hobbies: ['sports', 'cooking']
 };
+var favouriteActivities;
+favouriteActivities = ['Sports'];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+    // console.log(hobby.map());  // !! ERROR !!
+}
--- app.ts
+++ app.ts
@@ -1,10 +1,20 @@
 // const person: {
 //     name: string;
 //     age: number;
+//     hobbies: string[];
 // } = {
 const person = {
     name: 'Monica',
-    age: 25
+    age: 25,
+    hobbies: ['sports', 'cooking']
 };
 
-console.log(person.name)
+let favouriteActivities: string[];
+favouriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby);
+    // console.log(hobby.map());  // !! ERROR !!
+}
