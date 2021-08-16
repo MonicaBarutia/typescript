c4b42590f7a90c07e1de3ff32d439c70130f2cf4
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-16 17:37:07 +0300

    Tuples.

--- app.js
+++ app.js
@@ -6,8 +6,12 @@
 var person = {
     name: 'Monica',
     age: 25,
-    hobbies: ['sports', 'cooking']
+    hobbies: ['sports', 'cooking'],
+    role: [2, 'author']
 };
+// person.role.push('admin');   // Not error, it doesn't work with push()
+// person.role[1] = 10;     // Error
+// person.role = [0, 'admin', 'user'];   // Error
 var favouriteActivities;
 favouriteActivities = ['Sports'];
 console.log(person.name);
--- app.ts
+++ app.ts
@@ -3,11 +3,21 @@
 //     age: number;
 //     hobbies: string[];
 // } = {
-const person = {
+const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    role: [number, string];     // tuple
+} = {
     name: 'Monica',
     age: 25,
-    hobbies: ['sports', 'cooking']
+    hobbies: ['sports', 'cooking'],
+    role: [2, 'author']
 };
+
+// person.role.push('admin');   // Not error, it doesn't work with push()
+// person.role[1] = 10;     // Error
+// person.role = [0, 'admin', 'user'];   // Error
 
 let favouriteActivities: string[];
 favouriteActivities = ['Sports'];
