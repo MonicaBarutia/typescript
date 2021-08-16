e345814cfd71e6e7b879e8d299673da775f78e19
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-16 17:49:22 +0300

    Enums.

--- app.js
+++ app.js
@@ -3,11 +3,29 @@
 //     age: number;
 //     hobbies: string[];
 // } = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];     // tuple
+// } = {
+//     name: 'Monica',
+//     age: 25,
+//     hobbies: ['sports', 'cooking'],
+//     role: [2, 'author']
+// };
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+; // pot sa aiba si alte valori gen: ADMIN = 5, READ_ONLY = 'READ_ONLY
 var person = {
     name: 'Monica',
     age: 25,
     hobbies: ['sports', 'cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN
 };
 // person.role.push('admin');   // Not error, it doesn't work with push()
 // person.role[1] = 10;     // Error
@@ -19,4 +37,7 @@ for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
     // console.log(hobby.map());  // !! ERROR !!
+}
+if (person.role === Role.AUTHOR) {
+    console.log('is author');
 }
--- app.ts
+++ app.ts
@@ -3,16 +3,25 @@
 //     age: number;
 //     hobbies: string[];
 // } = {
-const person: {
-    name: string;
-    age: number;
-    hobbies: string[];
-    role: [number, string];     // tuple
-} = {
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+//     role: [number, string];     // tuple
+// } = {
+//     name: 'Monica',
+//     age: 25,
+//     hobbies: ['sports', 'cooking'],
+//     role: [2, 'author']
+// };
+
+enum Role { ADMIN , READ_ONLY, AUTHOR};     // pot sa aiba si alte valori gen: ADMIN = 5, READ_ONLY = 'READ_ONLY
+
+const person = {
     name: 'Monica',
     age: 25,
     hobbies: ['sports', 'cooking'],
-    role: [2, 'author']
+    role: Role.ADMIN,
 };
 
 // person.role.push('admin');   // Not error, it doesn't work with push()
@@ -27,4 +36,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
     console.log(hobby);
     // console.log(hobby.map());  // !! ERROR !!
+}
+
+if (person.role === Role.AUTHOR) {
+    console.log('is author')
 }
