02281ba87293c95cdd911ca90b0fc6776bd7ce3a
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-17 19:59:39 +0300

    Private and public access modifiers.

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,25 @@
 "use strict";
 class Department {
     constructor(name) {
+        this.employees = [];
         this.name = name;
     }
     describe() {
         console.log('Department: ' + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 const accounting = new Department('Accounting');
 console.log(accounting);
 accounting.describe();
-const accountingCopy = { name: 'Dummy', describe: accounting.describe };
-accountingCopy.describe();
+// const accountingCopy = { name: 'Dummy', describe: accounting.describe}
+// accountingCopy.describe();
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.printEmployeeInformation();
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
     name: string;
+    private employees: string[] = [];
 
     constructor(name: string) {
         this.name = name;
@@ -8,6 +9,15 @@ class Department {
     describe(this: Department) {
         console.log('Department: ' + this.name);
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Accounting');
@@ -15,6 +25,10 @@ console.log(accounting);
 
 accounting.describe();
 
-const accountingCopy = { name: 'Dummy', describe: accounting.describe}
-accountingCopy.describe();
+// const accountingCopy = { name: 'Dummy', describe: accounting.describe}
+// accountingCopy.describe();
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+accounting.printEmployeeInformation();
 
