9efbd95759d76fa87f0196946aa5965a8dae73c0
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-17 13:46:35 +0300

    The never type.

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
     username = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    // while(true) {}
+}
+generateError('En error occurred!', 500);
--- app.ts
+++ app.ts
@@ -7,3 +7,10 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
     username = userInput;
 }
+
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
+    // while(true) {}
+}
+
+generateError('En error occurred!', 500);
