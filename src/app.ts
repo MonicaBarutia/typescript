a9ec95844320f348e6a708b850b0ccf9fbe17465
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-17 18:35:25 +0300

    Arrow functions.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
-const button = document.querySelector('button')!;
+const add = (a: number, b: number = 1) => a + b;
 
-button.addEventListener('click', () => {
-    console.log("Clicked!")
-});
+const printOutput: (a: number | string) => void = output => console.log(output);
+
+const button = document.querySelector('button');
+
+if (button) {
+    button.addEventListener('click', event => console.log(event));
+}
+
+printOutput(add(5));
