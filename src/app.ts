21437488c0fc3a92f564f009552bb30ba1880a96
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-17 19:16:24 +0300

    Rest parameters.

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-const add = (a: number, b: number = 1) => a + b;
+// const add = (a: number, b: number = 1) => a + b;
 
 const printOutput: (a: number | string) => void = output => console.log(output);
 
@@ -8,7 +8,7 @@ if (button) {
     button.addEventListener('click', event => console.log(event));
 }
 
-printOutput(add(5));
+// printOutput(add(5));
 
 const hobbies = ['Sports', 'Cooking'];
 const activeHobbies = ['Hiking'];
@@ -21,3 +21,12 @@ const person = {
 };
 
 const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+
+const addedNumbers = add(5, 10, 2.8, 4);
+console.log(addedNumbers);
