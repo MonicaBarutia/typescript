13d7deec06e52bf4866eaa1ad1d168e1f144c1a3
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-18 14:11:15 +0300

    Intersection types.

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,21 @@
-// type AddFn = (a: number, b: number) =>  number;
-// interface AddFn {
-//     (a: number, b: number): number;
-// }
-//
-// let add: AddFn;
-// add = (n1: number, n2: number) => {
-//     return n1 + n2;
-// }
+type Admin = {
+  name: string;
+  privileges: string[];
+};
 
-interface Named {
-    readonly name?: string;
-    outputName?: string;
-}
+type Employee = {
+  name: string;
+  startDate: Date;
+};
 
-interface Greetable extends Named{
-    greet(phrase: string): void;
-}
+type ElevatedEmployee = Admin & Employee;
 
-class Person implements Greetable {
-    name?: string;
-    age = 27;
+const e1: ElevatedEmployee = {
+  name: 'John',
+  privileges: ['create-server'],
+  startDate: new Date()
+};
 
-    constructor(n?: string) {
-        if (n) {
-            this.name = n;
-        }
-    }
-
-    greet(phrase: string) {
-        if (this.name) {
-            console.log(phrase + ' ' + this.name);
-        } else {
-            console.log('Hi!');
-        }
-    }
-
-}
-
-let user1: Greetable;
-user1 = new Person();
-
-user1.greet('Hi there - I am');
-console.log(user1);
+type Combinable = string | number;
+type Numeric = number | boolean;
+type Universal = Combinable & Numeric;
