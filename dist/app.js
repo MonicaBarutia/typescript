4766ebf5a570bc0afc5e0267ea6b2a641941802f
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-18 17:45:32 +0300

    Generic classes.

--- dist/app.js
+++ dist/app.js
@@ -26,3 +26,34 @@ function extractAndConvert(obj, key) {
     return obj[key];
 }
 extractAndConvert({ name: 'Max' }, 'name');
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        if (this.data.indexOf(item) === -1) {
+            return;
+        }
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+textStorage.addItem('Max');
+textStorage.addItem('Manu');
+textStorage.removeItem('Max');
+console.log(textStorage.getItems());
+const numberStorage = new DataStorage();
+// const objStorage = new DataStorage<object>();
+// const maxObj = {name: 'Max'};
+// const manuObj = {name: 'Manu'};
+// objStorage.addItem(maxObj);
+// objStorage.addItem(manuObj);
+// // ...
+// objStorage.removeItem(maxObj);
+// console.log(objStorage.getItems());
