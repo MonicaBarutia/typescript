f792974616540fbc187b7119b0510a169d5edce3
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-18 14:47:41 +0300

    Discriminated unions.

--- src/app.ts
+++ src/app.ts
@@ -69,5 +69,31 @@ function useVehicle(vehicle: Vehicle) {
   }
 }
 
+interface Bird {
+  type: 'bird';
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: 'horse';
+  runningSpeed: number;
+}
+
 useVehicle(v1);
 useVehicle(v2);
+
+type Animal = Bird | Horse;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case 'bird':
+      speed = animal.flyingSpeed;
+      break;
+    case 'horse':
+      speed = animal.runningSpeed;
+  }
+  console.log('Moving with speed: ' + speed)
+}
+
+moveAnimal({type: 'bird', flyingSpeed: 10})
