c9ad39be5a331c3593522e9cea9a47c6dec8cddc
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-18 17:10:06 +0300

    Create generic function.

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,14 @@
 "use strict";
-const names = []; // string[]
-const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is done!');
-    }, 2000);
-});
+// const names: Array<string> = []; // string[]
+//
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('This is done!');
+//     }, 2000)
+// });
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
+}
+const mergedObj = merge({ name: 'Max' }, { age: 30 });
+const mergedObj2 = merge({ name: 'Max', hobbies: ['Sports'] }, { age: 30 });
+console.log(mergedObj.name);
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,15 @@
-const names: Array<string> = []; // string[]
+// const names: Array<string> = []; // string[]
+//
+// const promise: Promise<string> = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('This is done!');
+//     }, 2000)
+// });
 
-const promise: Promise<string> = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('This is done!');
-    }, 2000)
-});
+function merge<T, U>(objA: T, objB: U) {
+    return Object.assign(objA, objB)
+}
+
+const mergedObj = merge({name: 'Max'}, {age:30});
+const mergedObj2 = merge({name: 'Max', hobbies: ['Sports']}, {age:30});
+console.log(mergedObj.name);
