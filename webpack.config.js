3e3f6d728379550f206e04c336b008657611e660
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-25 12:04:40 +0300

    Get user input.

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,8 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist',
+    publicPath: 'dist'
   },
-  devtool: 'inline-source-map',
   module: {
     rules: [
       {
