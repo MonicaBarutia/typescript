061a85d4f526b2f9e7005fde688838b4799d0d48
Monica Barutia <monica.barutia @freshbyteinc.com>
2021-08-26 12:50:57 +0300

    Connect react components.

--- react-typescript/src/components/NewTodo.tsx
+++ react-typescript/src/components/NewTodo.tsx
@@ -1,23 +1,27 @@
 import React, { useRef } from 'react';
 
-const NewTodo: React.FC = () => {
-  const textInputRef = useRef<HTMLInputElement>(null);
+type NewTodoProps = {
+    onAddTodo: (todoText: string) => void;
+};
 
-  const todoSubmitHandler = (event: React.FormEvent) => {
-    event.preventDefault();
-    const enteredText = textInputRef.current!.value;
-    console.log(enteredText);
-  };
+const NewTodo: React.FC<NewTodoProps> = props => {
+    const textInputRef = useRef<HTMLInputElement>(null);
 
-  return (
-    <form onSubmit={todoSubmitHandler}>
-      <div>
-        <label htmlFor="todo-text">Todo Text</label>
-        <input type="text" id="todo-text" ref={textInputRef} />
-      </div>
-      <button type="submit">ADD TODO</button>
-    </form>
-  );
+    const todoSubmitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        const enteredText = textInputRef.current!.value;
+        props.onAddTodo(enteredText);
+    };
+
+    return (
+        <form onSubmit={todoSubmitHandler}>
+            <div>
+                <label htmlFor="todo-text">Todo Text</label>
+                <input type="text" id="todo-text" ref={textInputRef} />
+            </div>
+            <button type="submit">ADD TODO</button>
+        </form>
+    );
 };
 
 export default NewTodo;
